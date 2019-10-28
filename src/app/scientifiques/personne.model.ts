5dfab3fc2c2eb0590cb93978a97a0b71fec23e75
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-10-28 14:54:09 +0100

    Les scientifiques

---
+++ src/app/scientifiques/personne.model.ts
@@ -0,0 +1,9 @@
+export default class Personne {
+    constructor(public nom : string, 
+                public prenom : string, 
+                public decouverte : string, 
+                public description : string){
+
+    };
+
+}
---
+++ src/app/scientifiques/scientifiques.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import Personne from './personne.model';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './scientifiques.component.html',
+  styleUrls: ['./scientifiques.component.css']
+})
+export class ScientifiquesComponent implements OnInit {
+
+  titre : string = "Mes scientifiques";
+  personne : Personne = new Personne("Einstein","Albert","Relativité","Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptas sequi optio, dolores iure iusto ducimus enim, animi ipsum, quod nesciunt eveniet. Corporis exercitationem quas expedita ipsam earum facilis repellendus.");
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
